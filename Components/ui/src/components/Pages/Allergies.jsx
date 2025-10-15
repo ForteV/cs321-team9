a11820817a032476bb094e73ea0fb3e7621054a8
Victoria Klyushina <142938690+vklyushi@users.noreply.github.com>
2025-10-15 19:45:32 +0000

    Touch ups

--- Components/ui/src/components/Pages/Allergies.jsx
+++ Components/ui/src/components/Pages/Allergies.jsx
@@ -5,7 +5,7 @@ import "./Pages.css";
 
 export default function Allergies() {
     return (
-        <>
+        <div className={s.allergiesPage}>
             <section className={s.title}>
                 <title>Allergies</title>
                 <h1>Allergies</h1>
@@ -19,6 +19,6 @@ export default function Allergies() {
                 <Card items={["Immunization 2", "Date"]}/>
                 <Card items={["Immunization 3", "Date"]}/>
             </section>
-        </>
+        </div>
     );
 }
